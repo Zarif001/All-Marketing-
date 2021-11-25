739331db6ddfceecddfd8572686c62917c3c76a2
Zarifjon <zarifjon@gmail.com>
2021-11-25 11:36:30 +0500

    js

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-const aside = document.querySelector('.aside')
-aside.addEventListener('mouseover', () =>{
-aside.style.cssText = `
-width:234px;
-`
+const aside = document.querySelector('.aside'),
+header = document.querySelector('.header-content')
+aside.addEventListener('click', () =>{
+if(aside.classList.contains('active')){
+    aside.classList.remove('active')
+}
+else{
+    aside.classList.add('active')
+}
+})
+header.addEventListener('click', (e) =>{
+  if(e.target.classList.contains('header-content')){
+      aside.classList.remove('active')
+  }
 })
