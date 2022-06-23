9931e54c66a1e9ea0744dbb8005153ce4c57515a
zarif <zarif@icloud.com>
2022-06-23 21:29:10 +0500

    js

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const aside = document.querySelector('.aside'),
-header = document.querySelector('.header-content')
+header = document.querySelector('.header-content'),
+burger = document.querySelector('.aside-picture')
 aside.addEventListener('click', () =>{
+if(aside.classList.contains('active')){
+    aside.classList.remove('active')
+}
+else{
+    aside.classList.add('active')
+}
+})
+burger.addEventListener('click', () =>{
 if(aside.classList.contains('active')){
     aside.classList.remove('active')
 }
